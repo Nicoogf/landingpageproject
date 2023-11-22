805bd1c2631eb15768083bece7ea5a2fde2ad87a
Nicoogf <nicolasgfalabella@gmail.com>
2023-11-22 10:23:38 -0300

    Animaciones y Footer

--- js/main.js
+++ js/main.js
@@ -55,3 +55,27 @@ const swiper = new Swiper('.swiper', {
         });
     });
 });
+
+
+
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '60px',
+    duration: '3000',
+    delay : 600 ,
+    reset: true
+}) ;
+
+//
+
+sr.reveal('.hero__text' , { origin:'top' } )
+sr.reveal('.hero__img' )
+
+
+//stats
+sr.reveal('.stats__item' , {
+    delay:600,
+    distance : '100px' ,
+    interval: 100 ,
+    origin: 'top'
+})
